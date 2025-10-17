c09d8ca0523762867e007f61c45c12ab97d814fe
hkumarrai1 <166382429+hkumarrai1@users.noreply.github.com>
2025-10-17 19:02:40 +0530

    Fix some changes

--- Backend/main.py
+++ Backend/main.py
@@ -41,11 +41,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+        "https://supplierrag-n086dlt57-hkumarrai1s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
